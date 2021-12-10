890f5d9d9b21534e29656df55137727077c0a8a5
Myles Barrett <mylesb95@yahoo.com>
2021-12-09 23:42:41 -0700

    Initial commit

---
+++ book_day1.py
@@ -0,0 +1,17 @@
+
+
+print("Not gonna say hello world cause that'd be cringe... oh wait")
+
+cringe = "My name is Ivan"
+print(cringe)
+
+poop = "Lax Greenbax"
+print(poop.upper()) #this kinda cool, idk how useful it is
+print(poop.lower())
+
+first_name = "myles"
+last_name = "barrett"
+full_name = f"{first_name} {last_name}" #doing maths
+print(full_name)
+message = f"Hello, {full_name.title()}!"
+print(message)
